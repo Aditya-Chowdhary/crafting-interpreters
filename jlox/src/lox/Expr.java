9ba1030a2c768cb64c6d0425b058827c5b5faad5
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-03-16 22:54:47 +0530

    8.2 Global variable declaration

--- jlox/src/lox/Expr.java
+++ jlox/src/lox/Expr.java
@@ -1,13 +1,15 @@
 package lox;
 
+import java.util.List;
+
 abstract class Expr {
-  interface Visitor<R> {
+ interface Visitor<R> {
     R visitBinaryExpr(Binary expr);
     R visitGroupingExpr(Grouping expr);
     R visitLiteralExpr(Literal expr);
     R visitUnaryExpr(Unary expr);
-  }
-
+    R visitVariableExpr(Variable expr);
+ }
   static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
@@ -24,7 +26,6 @@ abstract class Expr {
     final Token operator;
     final Expr right;
   }
-
   static class Grouping extends Expr {
     Grouping(Expr expression) {
       this.expression = expression;
@@ -37,7 +38,6 @@ abstract class Expr {
 
     final Expr expression;
   }
-
   static class Literal extends Expr {
     Literal(Object value) {
       this.value = value;
@@ -50,7 +50,6 @@ abstract class Expr {
 
     final Object value;
   }
-
   static class Unary extends Expr {
     Unary(Token operator, Expr right) {
       this.operator = operator;
@@ -65,6 +64,18 @@ abstract class Expr {
     final Token operator;
     final Expr right;
   }
+  static class Variable extends Expr {
+    Variable(Token name) {
+      this.name = name;
+    }
 
-  abstract <R> R accept(Visitor<R> visitor);
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitVariableExpr(this);
+    }
+
+    final Token name;
+  }
+
+ abstract <R> R accept(Visitor<R> visitor);
 }

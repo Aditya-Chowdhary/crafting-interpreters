284e9abb029ed06fe0c6cdb5a147d395120895bd
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-08-08 01:51:22 +0530

    16.1 Init repl and scanner

--- clox/vm.h
+++ clox/vm.h
@@ -7,23 +7,22 @@
 #define STACK_MAX 256
 
 typedef struct {
-    Chunk* chunk;
-    uint8_t* ip;
-    Value stack[STACK_MAX];
-    Value* stackTop;
+  Chunk *chunk;
+  uint8_t *ip;
+  Value stack[STACK_MAX];
+  Value *stackTop;
 } VM;
 
 typedef enum {
-    INTERPRET_OK,
-    INTERPRET_COMPILER_ERROR,
-    INTERPRET_RUNTIME_ERROR
+  INTERPRET_OK,
+  INTERPRET_COMPILER_ERROR,
+  INTERPRET_RUNTIME_ERROR
 } InterpretResult;
 
 void initVM();
 void freeVM();
-InterpretResult interpret(Chunk* chunk);
+InterpretResult interpret(const char *source);
 void push(Value value);
 Value pop();
-
 
 #endif
